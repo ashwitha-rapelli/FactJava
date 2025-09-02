3d9daf5deeea828fa87c8a46cddcc005dfab293f
ashwitha-rapelli <ashwitharapelli10@gmail.com>
2025-09-02 09:37:42 +0530

    first commit

---
+++ src/main/java/com/example/FactJava/App.java
@@ -0,0 +1,19 @@
+package com.example.FactJava;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int n=6;
+        int f=1;
+        for(int i=1;i<=n;i++) {
+        	f=f*i;
+        }
+        System.out.println(f);
+    }
+}
